c35bb23342d5ed6c0d2642abdc8231da3b2339ff
bparker12 <49100894+bparker12@users.noreply.github.com>
2020-01-23 23:53:18 -0600

    add name field to inventory item

--- vendingTestapi/models/inventory.py
+++ vendingTestapi/models/inventory.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Inventory(models.Model):
 
+    name = models.CharField(max_length=50)
     quantity = models.IntegerField(default=5)
     price = models.IntegerField(default=2)
