8dc49cbbe4129e5dd0c2721984a9dce621a3acfb
bparker12 <49100894+bparker12@users.noreply.github.com>
2020-01-24 00:16:27 -0600

    fixtures work, and Reset_DB resets db and loads fixtures

--- Reset_db.sh
+++ Reset_db.sh
@@ -2,6 +2,8 @@ rm -rf budgetappapi/migrations
 rm db.sqlite3
 python manage.py makemigrations vendingTestapi
 python manage.py migrate
+python manage.py loaddata coin
+python manage.py loaddata inventory
 
 # 1. Save that in a Reset_db.sh file
 # 2. Run chmod +x Reset_db.sh (in bash only not Windows Terminal)
