6c195a5659b3ebad38f605a652a91db15f50ad92
bparker12 <49100894+bparker12@users.noreply.github.com>
2020-01-24 00:09:40 -0600

    fixtures update to include default values & Reset update

--- Reset_db.sh
+++ Reset_db.sh
@@ -2,3 +2,7 @@ rm -rf budgetappapi/migrations
 rm db.sqlite3
 python manage.py makemigrations vendingTestapi
 python manage.py migrate
+
+# 1. Save that in a Reset_db.sh file
+# 2. Run chmod +x Reset_db.sh (in bash only not Windows Terminal)
+# 3. Run ./Reset_db.sh
--- vendingTestapi/models/inventory.py
+++ vendingTestapi/models/inventory.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Inventory(models.Model):
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default="")
     quantity = models.IntegerField(default=5)
     price = models.IntegerField(default=2)
