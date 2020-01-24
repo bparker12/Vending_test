307208eb594debcee18aa3049484071c49afe6e7
bparker12 <49100894+bparker12@users.noreply.github.com>
2020-01-24 13:20:08 -0600

    good start to inventory transactions

--- vendingTestapi/views/inventory.py
+++ vendingTestapi/views/inventory.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from rest_framework import status
 from vendingTestapi.models import Inventory
 from vendingTestapi.models import Coin
+from .coin import CoinSerializer
 
 
 class InventorySerializer(serializers.HyperlinkedModelSerializer):
@@ -38,5 +39,32 @@ class Inventories(ViewSet):
         return Response(serializer.data["quantity"], status=status.HTTP_200_OK)
 
     def update(self, request, pk=None):
+
         inventory = Inventory.objects.get(pk=pk)
-        print(inventory)
+
+        coin = Coin.objects.get(pk=1)
+
+        # inv_serializer = InventorySerializer(inventory, context={'request': request})
+        # coin_serializer = CoinSerializer(coin, context={'request': request})
+
+        # inv_price = inv_serializer.data["price"]
+        # inv_qty = inv_serializer.data["quantity"]
+        # coin_qty = (coin_serializer.data["quantity"])
+
+        inv_price = inventory.price
+        inv_qty = inventory.quantity
+        coin_qty = coin.quantity
+
+        if inv_qty > 0:
+            if inv_price > coin_qty:
+                coins_short = inv_price - coin_qty
+                print(coins_short)
+                return Response(status=status.HTTP_403_FORBIDDEN, headers={"X-Coins": coins_short})
+            else:
+                inventory.quantity = inv_qty - 1
+                inv_remaining = inv_qty
+                coin_return = coin_qty - inv_price
+                coin.quantity = 0
+                inventory.save()
+                coin.save()
+                return Response(status=status.HTTP_200_OK, headers={"X-Coins": coin_return})
